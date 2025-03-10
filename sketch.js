399614789373e18196e239440c6d86ee0f668b25
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-03-10 17:50:37 +0100

    add resizing

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  rect(0, 0, 100, 100);
+  const s = 100;
+
+  rect(0, 0, s, s);
+  rect(0, height - s, s, s);
+  rect(width - s, 0, s, s);
+  rect(width - s, height - s, s, s);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
